b7b036287cc2c89951ee17cf6d462a3c1cd79e4a
Marcelo <maxcelu@yahoo.com.br>
2021-08-30 02:07:00 -0300

    modificando

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,11 @@ const connectToDatabase = require ('./database')
 
 connectToDatabase()
 const app = express ()
-const port = 5000
 
 
 app.use(cors())
 app.use(express.json())
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use("/", routes)
-app.listen(process.env.PORT || port, ()=> {console.log('rodando...')})
+app.listen( process.env.PORT, ()=> {console.log('rodando...')})
 
