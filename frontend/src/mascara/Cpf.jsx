3cca58a187b1c1320fd7099e7cc7e1b16993c084
Marcelo <maxcelu@yahoo.com.br>
2021-09-07 16:25:39 -0300

    correcao de formatacao de documentos

--- frontend/src/mascara/Cpf.jsx
+++ frontend/src/mascara/Cpf.jsx
@@ -1,8 +1,9 @@
 export function cpf(e) {
     e.currentTarget.maxLength = 14
     let value = e.currentTarget.value
-    value = value.replace(/\D/g, "")
-    value = value.replace(/^(\d{3})+(\d{3})+(\d{3})+(\d{2})$/, "$1.$2.$3-$4" )
+    // value = value.replace(/\D/g, "")
+    value = value.replace(/^(\d{3})+(\d{3})+(\d{3})/, "$1.$2.$3-" )
+    // value = value.replace(/^(\d{3})+(\d{3})+(\d{3})+(\d{2})$/, "$1.$2.$3-$4" )
     e.currentTarget.value = value
     return e
 }
--- frontend/src/mascara/Rg.jsx
+++ frontend/src/mascara/Rg.jsx
@@ -2,7 +2,8 @@ export function identidade(e) {
     e.currentTarget.maxLength = 12
     let value = e.currentTarget.value
     value = value.replace(/\D/g, "")
-    value = value.replace(/^(\d{2})+(\d{3})+(\d{3})+(\d{1})$/, "$1.$2.$3-$4" )
+    value = value.replace(/^(\d{2})+(\d{3})+(\d{3})/, "$1.$2.$3-" )
+    // value = value.replace(/^(\d{2})+(\d{3})+(\d{3})+(\d{1})$/, "$1.$2.$3-$4" )
     e.currentTarget.value = value
     return e
 }
